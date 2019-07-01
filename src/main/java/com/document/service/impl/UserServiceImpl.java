2b3078acba7ec1111b4de8238de201d3b29870fa
LM123Code <1456466514@qq.com>
2019-07-01 16:12:45 +0800

    登录方法api：http://localhost:8080/user/login，只能post方法请求

---
+++ src/main/java/com/document/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.document.service.impl;
+
+import com.document.mapper.UserMapper;
+import com.document.pojo.User;
+import com.document.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author LM_Code
+ * @create 2019-07-01-15:57
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public User login(String userId, String password) {
+        return userMapper.selectLogin(userId, password);
+    }
+}
