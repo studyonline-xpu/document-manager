247ab77710815d5fc322ab97006f905a527e6d0a
Lemon <1501901753@qq.com>
2019-07-02 08:53:46 +0800

    注册登录测试完成

--- src/main/java/com/document/controller/UserController.java
+++ src/main/java/com/document/controller/UserController.java
@@ -24,8 +24,10 @@ public class UserController {
     public String login(String userId, String password, Model model){
         User user = userService.login(userId, password);
         SystemResult result;
-        if(user != null)
-            return "index/dashboard";
+        if(user != null){
+            System.out.println(user);
+            return "redirect:/main.html";
+        }
         else{
             model.addAttribute("msg", "账号或密码错误!");
             return "login";
