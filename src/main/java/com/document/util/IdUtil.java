9d3770a4125b1ac9a2a33e74ffe36f115457e386
rz0310 <1362024331@qq.com>
2019-07-02 16:11:27 +0800

    完成根据DocumentName查询文档信息：http://localhost:8080/document/queryByDocumentName?documentName=aa 补充了用户注册时的时间

--- src/main/java/com/document/util/IdUtil.java
+++ src/main/java/com/document/util/IdUtil.java
@@ -3,10 +3,17 @@ package com.document.util;
 import com.relops.snowflake.Snowflake;
 import org.apache.commons.lang3.RandomUtils;
 
+/**
+ * 生成用户ID的工具类
+ */
 public class IdUtil {
 
     private static Snowflake s = new Snowflake(RandomUtils.nextInt(0, 1024));
 
+    /**
+     * 生成用户id的方法
+     * @return
+     */
     public static String next(){
         String id = s.next() + "";
         String pre = id.substring(0, 10);
